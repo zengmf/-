62ec30e4f362beb1d22b06359264150dfc833032
zengmf <46768649+zengmf@users.noreply.github.com>
2019-02-25 17:17:00 +0800

    Update refresh.java

--- refresh.java
+++ refresh.java
@@ -6,7 +6,7 @@ public class refresh {
 	public static void RefreshReading(JTextField textField,JTextField textField_1) 
 			throws InterruptedException {
 		String url = textField.getText();
-		for(int i=0;i<=Integer.valueOf(textField_1.getText());i++){
+		for(int i=0;i<Integer.valueOf(textField_1.getText());i++){
 			System.out.println(i);
 			HttpUtil.sendGet(url, null);
 			Thread.sleep(10);
